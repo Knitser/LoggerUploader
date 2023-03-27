be94ea7a01297bd91bbf4cd5cf4584171c41caec
Michiel <44088477+Knitser@users.noreply.github.com>
2023-03-27 13:03:40 +0200

    serial script

---
+++ serialread.py
@@ -0,0 +1,20 @@
+import serial
+
+# Define the serial port settings
+port = 'COM1'
+baudrate = 115200
+
+# Create a new serial object
+ser = serial.Serial(port, baudrate)
+
+while True:
+    try:
+        line = ser.readline()
+
+        # Decode the bytes into a string
+        # line = line.decode('utf-8').strip()
+
+        print(line)
+    except KeyboardInterrupt:
+        ser.close()
+        break
