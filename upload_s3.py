1c9d3cd05a33342fd816e8fc7f43696eefc10bd6
Michiel <44088477+Knitser@users.noreply.github.com>
2023-03-27 15:02:00 +0200

    Changed chunk to file, but it still splits them into chunks

--- upload_s3.py
+++ upload_s3.py
@@ -10,11 +10,11 @@ class S3Uploader:
         self.bucket_name = bucket_name
         self.s3_prefix = s3_prefix
 
-    def upload_chunk(self, filepath):
+    def upload_file(self, filepath):
         with open(filepath, 'rb') as f_in:
             filename = os.path.basename(filepath)
             self.s3.upload_fileobj(f_in, self.bucket_name, self.s3_prefix + filename)
-            print(f'Chunk {filename} ({os.path.getsize(filepath)} bytes) successfully uploaded to S3')
+            print(f'file {filename} ({os.path.getsize(filepath)} bytes) successfully uploaded to S3')
 
 
 class LogfileSplitter:
