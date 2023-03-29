3a18ac28a1f2a11842675ef00585135a059cea5c
Michiel Hamblok <michielhamblok@gmail.com>
2023-03-29 14:50:06 +0200

    added save logging to logfile directory

--- serialread.py
+++ serialread.py
@@ -1,22 +1,33 @@
 import serial
+import logging
+import time
 
 # Define the serial port settings
 # port = 'COM1'
 port = '/dev/ttyUSB0'
 baudrate = 115200
 
+# Configure the logger to write to a file in the logfiles directory
+logging.basicConfig(filename='logfiles/logfile.asc', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
 # Create a new serial object
 ser = serial.Serial(port, baudrate)
-# ser = serial.Serial('/dev/ttyAMA0', 115200)
 
 while True:
     try:
         line = ser.readline()
 
         # Decode the bytes into a string
-        # line = line.decode('utf-8').strip()
+        line = line.decode('utf-8').strip()
+
+        # Write the line to the log file
+        logging.info(line)
 
         print(line)
+
+        # Sleep for 2 minutes
+        time.sleep(120)
+
     except KeyboardInterrupt:
         ser.close()
         break
