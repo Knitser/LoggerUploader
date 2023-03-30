86e94e9e28d5b7118ac3e06d6ae5200ff3972b0b
Michiel Hamblok <michielhamblok@gmail.com>
2023-03-30 19:07:50 +0200

    test

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 from serialsimple import SerialLogger
 
 def main():
-    logger = SerialLogger(port='/dev/ttyACM1', baudrate=115200, log_interval_sec=5, log_directory='logfiles', zip_directory='zipfiles')
+    logger = SerialLogger(port='/dev/ttyACM0', baudrate=115200, log_interval_sec=5, log_directory='logfiles', zip_directory='zipfiles')
     logger.start_logging()
 
 if __name__ == '__main__':
--- serialsimple.py
+++ serialsimple.py
@@ -11,7 +11,7 @@ class SerialLogger:
         self.log_directory = log_directory
         self.zip_directory = zip_directory
 
-        # Create a new directory for the log files if it doesn't exist
+   # Create a new directory for the log files if it doesn't exist
         if not os.path.exists(self.log_directory):
             os.makedirs(self.log_directory)
 
@@ -49,14 +49,14 @@ class SerialLogger:
                     # rename the current log file to the new filename
                     os.rename(log_filename, new_log_filename)
 
-                    # zip the old log file
-                    self.zip_log_file(new_log_filename)
-
                     # open a new log file for writing
                     log_file = open(log_filename, 'w')
 
                     # reset the start time
                     start_time = time.time()
+
+                    # zip the previous log file
+                    self.zip_logs(new_log_filename)
 
         except KeyboardInterrupt:
             ser.close()
@@ -65,15 +65,7 @@ class SerialLogger:
     def _get_log_filename(self):
         return os.path.join(self.log_directory, time.strftime("%Y-%m-%d_%H-%M-%S_logfile.asc", time.localtime()))
 
-    def zip_log_file(self, log_filename):
-        # create the gzip filename based on the log filename
-        gzip_filename = os.path.splitext(os.path.basename(log_filename))[0] + '.gz'
-
-        # open the log file for reading
-        with open(log_filename, 'rb') as f_in:
-            # create the gzip file for writing
-            with gzip.open(os.path.join(self.zip_directory, gzip_filename), 'wb') as f_out:
-                f_out.writelines(f_in)
-
-        # delete the log file
-        os.remove(log_filename)
+    def zip_logs(self, log_filename):
+        zip_filename = os.path.join(self.zip_directory, os.path.basename(log_filename) + '.gz')
+        with open(log_filename, 'rb') as f_in, gzip.open(zip_filename, 'wb') as f_out:
+            f_out.writelines(f_in)
