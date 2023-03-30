a983814fe57614330ac3012cd063bf64f219f077
Michiel Hamblok <michielhamblok@gmail.com>
2023-03-30 23:41:43 +0200

    Final testing

--- main2.py
+++ main2.py
@@ -1,7 +1,18 @@
 from serialsimple import SerialLogger
+from datetime import datetime
 
 def main():
-    logger = SerialLogger(port='/dev/ttyACM0', baudrate=115200, log_interval_sec=5, log_directory='logfiles', zip_directory='zipfiles')
+
+    port='/dev/ttyACM0'
+    baudrate=115200
+    log_interval_sec=120
+    log_directory='logfiles'
+    zip_directory='zipfiles'
+    bucket_name = 'blackboxlinkedcar'
+    s3_prefix = datetime.now().strftime('logfiles/%Y/%m/%d/%H/')
+    timer = 600
+    
+    logger = SerialLogger(port, baudrate, log_interval_sec, log_directory, zip_directory, bucket_name, s3_prefix, timer)
     logger.start_logging()
 
 if __name__ == '__main__':
--- serialsimple.py
+++ serialsimple.py
@@ -2,33 +2,50 @@ import serial
 import time
 import os
 import gzip
+from upload_s3 import S3Uploader
+import threading
+
 
 class SerialLogger:
-    def __init__(self, port, baudrate, log_interval_sec, log_directory, zip_directory):
+    def __init__(self, port, baudrate, log_interval_sec, log_directory, zip_directory, bucket_name, s3_prefix, timer):
         self.port = port
         self.baudrate = baudrate
         self.log_interval_sec = log_interval_sec
         self.log_directory = log_directory
         self.zip_directory = zip_directory
+        self.s3_uploader = S3Uploader(bucket_name, s3_prefix)
+        self.timer = timer
 
-   # Create a new directory for the log files if it doesn't exist
         if not os.path.exists(self.log_directory):
             os.makedirs(self.log_directory)
 
-        # Create a new directory for the zip files if it doesn't exist
         if not os.path.exists(self.zip_directory):
             os.makedirs(self.zip_directory)
 
+    def upload_and_delete_zip_files(self):
+        for zip_file in os.listdir(self.zip_directory):
+            zip_filepath = os.path.join(self.zip_directory, zip_file)
+            self.s3_uploader.upload_file(zip_filepath)
+            os.remove(zip_filepath)
+
+    def _get_log_filename(self):
+        return os.path.join(self.log_directory, time.strftime("%Y-%m-%d_%H-%M-%S_logfile.asc", time.localtime()))
+
+    def zip_logs(self, log_filename):
+        zip_filename = os.path.join(self.zip_directory, os.path.basename(log_filename) + '.gz')
+        with open(log_filename, 'rb') as f_in, gzip.open(zip_filename, 'wb') as f_out:
+            f_out.writelines(f_in)
+
     def start_logging(self):
-        # Create a new serial object
         ser = serial.Serial(self.port, self.baudrate)
 
-        # Create a new log file
         log_filename = self._get_log_filename()
         log_file = open(log_filename, 'w')
 
-        # set the start time
         start_time = time.time()
+
+        upload_timer = threading.Timer(self.timer, self.upload_and_delete_zip_files)
+        upload_timer.start()
 
         try:
             while True:
@@ -58,14 +75,12 @@ class SerialLogger:
                     # zip the previous log file
                     self.zip_logs(new_log_filename)
 
+                # upload to s3 file.
+                if time.time() - start_time >= 10:
+                    self.upload_and_delete_zip_files()
+                    start_time = time.time()
+
         except KeyboardInterrupt:
             ser.close()
             log_file.close()
 
-    def _get_log_filename(self):
-        return os.path.join(self.log_directory, time.strftime("%Y-%m-%d_%H-%M-%S_logfile.asc", time.localtime()))
-
-    def zip_logs(self, log_filename):
-        zip_filename = os.path.join(self.zip_directory, os.path.basename(log_filename) + '.gz')
-        with open(log_filename, 'rb') as f_in, gzip.open(zip_filename, 'wb') as f_out:
-            f_out.writelines(f_in)
