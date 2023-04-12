54a457f68c41dd274597139bc5af3207c393b2d2
Michiel <44088477+Knitser@users.noreply.github.com>
2023-04-12 12:39:01 +0200

    changed esp flash file to class and basic function

--- flash.py
+++ flash.py
@@ -1,20 +1,30 @@
-import esptool
+import subprocess
 
-port = "/dev/ttyUSB0"
-baudrate = 115200
-chip_type = "esp32"
-firmware_path = "/path/to/firmware.bin"
 
-# Create an instance of the esptool.ESP32Serial class
-esp = esptool.ESP32Serial(port=port, baudrate=baudrate)
+class EspFlasher:
+    def __init__(self, port, baudrate, chip_type):
+        self.port = port
+        self.baudrate = baudrate
+        self.chip_type = chip_type
 
-esptool.write_flash(
-    esp,
-    0,
-    firmware_path,
-    chip_type=chip_type,
-    verify=True,
-    erase_all=True
-)
+    def flash_firmware(self, firmware_path):
+        cmd = [
+            "esptool.exe",
+            "--chip", self.chip_type,
+            "--port", self.port,
+            "--baud", str(self.baudrate),
+            "write_flash", "0x0", firmware_path
+        ]
+        print(cmd)
 
-print("Firmware flashed successfully!")
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if result.returncode != 0:
+            print("Error flashing firmware:")
+            print(result.stderr.decode())
+        else:
+            print("Firmware flashed successfully!")
+
+
+flasher = EspFlasher(port="COM5", baudrate=115200, chip_type="esp32")
+flasher.flash_firmware(firmware_path="firmware.bin")
